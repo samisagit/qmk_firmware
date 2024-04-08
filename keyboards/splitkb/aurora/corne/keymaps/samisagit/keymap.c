ec3d2831f3dd890a2421e57f505f9bf48cabd120
Sam White <sam.white@nandos.co.uk>
2024-04-08 09:26:25 +0100

    Swap hash and slash

--- keyboards/splitkb/aurora/corne/keymaps/samisagit/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/samisagit/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TAB, LCTL_T(KC_A),    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, RCTL_T(KC_SCLN), KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_BSLS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_HASH,  KC_SLSH,
+      KC_BSLS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_HASH,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
       KC_LGUI,   LSFT_T(KC_ENT),  TD(TD_MOD_1_3),                                           TD(TD_MOD_2_4),   RSFT_T(KC_SPC), KC_LALT
                                       //`--------------------------'  `--------------------------'
